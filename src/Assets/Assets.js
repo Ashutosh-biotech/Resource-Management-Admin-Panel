02861d5aca2b306dbc2e42b50289115db64ee85f
Ashutosh Kumar <ashutoshbiotech2020@gmail.com>
2024-08-27 15:12:20 +0530

    navbar completed

--- src/Assets/Assets.js
+++ src/Assets/Assets.js
@@ -1,3 +1,4 @@
 import Logo from "./images/NxtWave TM_Coloured logo 1.svg";
+import Me from "./images/Picture4.jpg"
 
-export {Logo};
+export {Logo, Me};
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,35 @@
-import {Logo} from "../Assets/Assets"
+import {Link} from "react-router-dom";
+import {Logo, Me} from "../Assets/Assets";
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-light bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    <img
-                        src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-                        height="20"
-                        alt="MDB Logo"
-                        loading="lazy"
-                    />
-                </a>
-                <button data-mdb-collapse-init className="navbar-toggler" type="button" data-mdb-target="#navbarButtonsExample" aria-controls="navbarButtonsExample" aria-expanded="false" aria-label="Toggle navigation">
-                    <i className="fas fa-bars"></i>
-                </button>
-            </div>
-        </nav>
+        <>
+            <nav className="navbar navbar-expand-sm navbar-light bg-white">
+                <div className="container-fluid">
+                    <div className={"navbar-brand"}>
+                        {/*<Link to="/">*/}
+                        <img src={Logo} alt="Next Wave"/>
+                        {/*</Link>*/}
+                    </div>
+                    <div className={"d-flex align-items-center"}>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav ms-auto align-items-center">
+                                <li className="nav-item">
+                                    <button type="button" className="btn btn-success btn-sm">Add Item</button>
+                                </li>
+                                <li className="nav-item dropdown">
+                                    <img src={Me} alt="" className={"img-fluid rounded-circle ml-4"} style={{width: "40px"}}/>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
     )
 }
